9d94448175a756c1ae75e021a14ef67c36149c35
eduprivate <edu.private@gmail.com>
2023-11-17 20:30:27 -0300

    sliding windown

---
+++ src/main/java/br/eduprivate/problems_solving/sliding/GFG.java
@@ -0,0 +1,31 @@
+package br.eduprivate.problems_solving.sliding;
+
+class GFG {
+    // Returns maximum sum in
+    // a subarray of size k.
+    private static int maxSum(int arr[], int k)
+    {
+        // Initialize result
+        int max_sum = Integer.MIN_VALUE;
+
+        // Consider all blocks starting with i.
+        for (int i = 0; i < arr.length - k + 1; i++) {
+            int current_sum = 0;
+            for (int j = 0; j < k; j++)
+                current_sum = current_sum + arr[i + j];
+
+            // Update result if required.
+            max_sum = Math.max(current_sum, max_sum);
+        }
+
+        return max_sum;
+    }
+
+    // Driver code
+    public static void main(String[] args)
+    {
+        int arr[] = { 1, 4, 2, 10, 2, 3, 1, 0, 20 };
+        int k = 4;
+        System.out.println(maxSum(arr, k));
+    }
+}
---
+++ src/main/java/br/eduprivate/problems_solving/sliding/GFG3.java
@@ -0,0 +1,31 @@
+package br.eduprivate.problems_solving.sliding;
+
+public class GFG3 {
+
+
+    private static int maxSum(int[] arr, int k) {
+        if (k > arr.length){
+            return -1;
+        }
+        int max_sum = 0;
+
+        for (int i = 0; i < k; i++) {
+            max_sum += arr[i];
+        }
+
+        int window_sum = max_sum;
+        for (int i = k; i < arr.length; i++){
+            max_sum += arr[i] - arr[i-k];
+            window_sum = Math.max(window_sum, max_sum);
+        }
+        return window_sum;
+    }
+    public static void main(String[] args)
+    {
+        int arr[] = { 1, 4, 2, 10, 2, 3, 1, 0, 20, 2, 3, 4, 20, 4, 6, 2, 0, 40 };
+        int k = 4;
+        int n = arr.length;
+        System.out.println(maxSum(arr, k));
+    }
+
+}
