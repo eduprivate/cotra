774f4d1e954e47900acb47771a74520ba0ddfb2e
eduprivate <edu.private@gmail.com>
2023-12-08 09:59:38 -0300

    some problems, easy

--- src/main/java/br/eduprivate/problems_solving/RemoveDuplicates.java
+++ src/main/java/br/eduprivate/problems_solving/RemoveDuplicates.java
@@ -6,31 +6,81 @@ import java.util.List;
 import java.util.Set;
 
 public class RemoveDuplicates {
+
+    static int removeDuplicates2(int arr[])
+    {
+        int n = arr.length;
+
+        if (n == 0 || n == 1)
+            return n;
+
+        int[] temp = new int[n];
+
+        int j = 0;
+        for (int i = 0; i < n - 1; i++)
+            if (arr[i] != arr[i + 1])
+                temp[j++] = arr[i];
+
+
+        temp[j++] = arr[n - 1];
+
+        for (int i = 0; i < j; i++)
+            arr[i] = temp[i];
+
+
+        return j-1;
+    }
+
+    static int removeDuplicates(int arr[], int n)
+    {
+        if (n == 0 || n == 1)
+            return n;
+
+        int[] temp = new int[n];
+
+        int j = 0;
+        for (int i = 0; i < n - 1; i++)
+            if (arr[i] != arr[i + 1])
+                temp[j++] = arr[i];
+
+
+        temp[j++] = arr[n - 1];
+
+        for (int i = 0; i < j; i++)
+            arr[i] = temp[i];
+
+
+        return j-1;
+    }
     public int removeDuplicates(int[] nums) {
-
+        List<Integer> result = new ArrayList<>();
         int size = nums.length;
-        for (int i = 0; i < nums.length-1; i++) {
-            if(nums[i] == nums[i+1]) {
-                nums[i+1] = -1000;
-                size--;
+        for (int i = 0; i < nums.length; i++) {
+            int actual = nums[i];
+            if(result.contains(actual)) {
+                continue;
+            } else {
+                result.add(actual);
             }
         }
 
-        for (int i = 0; i < nums.length-1; i++) {
-            if(nums[i] == -1000) {
-                int temp = nums[i+1];
-                nums[i] = temp;
-                nums[i+1] = -1000;
+        for (int i = 0; i < nums.length; i++) {
+            if (i < result.size()) {
+                nums[i] = result.get(i);
+            } else {
+                nums[i] = -1000;
             }
+
         }
 
-        return size;
+        return result.size();
     }
 
     public static void main(String[] args)
     {
-        int[] s = new int[]{1,1,2};
+        int[] s = new int[]{0,0,1,1,1,2,2,3,3,4};
         RemoveDuplicates removeDuplicates = new RemoveDuplicates();
         System.out.println(removeDuplicates.removeDuplicates(s));
+        System.out.println(removeDuplicates.removeDuplicates(s, s.length));
     }
 }
