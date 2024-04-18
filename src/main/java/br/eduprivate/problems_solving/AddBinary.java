ba801c4242a140b1f19b7e04bd4a69ed4cfe0cd1
eduprivate <edu.private@gmail.com>
2024-04-18 15:02:03 -0300

    plus one problem

---
+++ src/main/java/br/eduprivate/problems_solving/AddBinary.java
@@ -0,0 +1,19 @@
+package br.eduprivate.problems_solving;
+
+import java.util.Arrays;
+
+public class AddBinary {
+    public static String addBinary(String a, String b) {
+
+        return "";
+    }
+
+    public static void main(String[] args) {
+
+        String a = "11";
+        String b = "1";
+
+        AddBinary addBinary = new AddBinary();
+        System.out.println(addBinary(a, b));
+    }
+}
--- src/main/java/br/eduprivate/problems_solving/PlusOne.java
+++ src/main/java/br/eduprivate/problems_solving/PlusOne.java
@@ -5,20 +5,25 @@ import java.util.Arrays;
 public class PlusOne {
 
     public int[] plusOne(int[] digits) {
-        for (int i = digits.length-1; i >=0 ; i--) {
+        for (int i = digits.length - 1; i >= 0; i--) {
             if (digits[i] < 9) {
                 digits[i]++;
+                return digits;
             }
             digits[i] = 0;
         }
 
-        int digits2[] = new int[digits.length + 1];
-        digits2[0] = 1;
-        return digits2;
+        digits = new int[digits.length + 1];
+        digits[0] = 1;
+        return digits;
     }
 
 
     public static void main(String[] args) {
+
+        String s1 = "ah5f1b1h86";
+        System.out.println(s1.getBytes().length);
+
         int[] digits = {1,9};
         PlusOne plusOne = new PlusOne();
         System.out.println(Arrays.toString(plusOne.plusOne(digits)));
